6ed44d63215191418537c436512e8c8012ca9eda
Your Name <you@example.com>
2025-12-11 14:30:20 +0800

    Change local port to 3005 to avoid conflicts

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
     server: {
-      port: 3000,
+      port: 3005,
       host: '0.0.0.0',
       proxy: {
         '/zhipu-api': {
